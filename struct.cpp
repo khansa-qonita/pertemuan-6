f12f699f15c71dd20935375282177c34e5d4c52a
khansa-qonita <khansaqonita420@gmail.com>
2026-04-07 18:20:47 +0700

    Mendefinisikan struct sebelum fungsi main

--- struct.cpp
+++ struct.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+struct orang 
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
