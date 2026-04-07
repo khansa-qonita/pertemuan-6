d2a2adcd56d5fefc55420dc4304275f55acf59b8
khansa-qonita <khansaqonita420@gmail.com>
2026-04-07 18:53:10 +0700

    mendeklarasikan variabel didalam fungsi utama

--- structgetline.cpp
+++ structgetline.cpp
@@ -14,3 +14,19 @@ struct Orang
     detailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    //deklarasi variabel struct
+    Orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    getline(cin , mhs.nama);
+    cout << "Isikan data desa : ";
+    getline(cin , mhs.alamat.desa);
+    cout << "Isikan data kota : ";
+    getline(cin ,  mhs.alamat.kota);
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
